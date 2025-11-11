a3a2bdb85389ee508e61d667ea7a6e65f7c40a46
Bax-47 <pritsoni.47.ps@gmail.com>
2025-11-11 10:28:43 -0800

    Initial commit: calculator app, tests, Jenkinsfile

---
+++ tests/test_calculator.py
@@ -0,0 +1,18 @@
+import pytest
+from app.calculator import add, sub, mul, div
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_sub():
+    assert sub(10, 4) == 6
+
+def test_mul():
+    assert mul(6, 7) == 42
+
+def test_div():
+    assert div(9, 3) == 3
+
+def test_div_by_zero():
+    with pytest.raises(ValueError):
+        div(1, 0)
